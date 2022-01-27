e4366d58a9b75334c052cd6431e0fd4716919b25
kofi-teddy <agudogot@gmail.com>
2022-01-27 23:37:09 +0000

    linting

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,8 @@
 import os
+from pathlib import Path
 
 import dj_database_url
 from decouple import config
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,9 +96,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
 from django.conf import settings
-
 from django.contrib import admin
 from django.urls import path
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
