4edf19ebdf981645b13087af1fbbe3d5487e1cfa
kofi-teddy <agudogot@gmail.com>
2022-02-03 13:55:55 +0000

    fix models

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=255, db_index=True)
 
     class Meta:
         ordering = ('name',)
@@ -17,10 +17,10 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SLugField(max_length=255, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    slug = models.SlugField(max_length=255, db_index=True)
+    # image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_length=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
