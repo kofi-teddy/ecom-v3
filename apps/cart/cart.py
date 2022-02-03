b6be5c420aeb7b736534b5bff9579b2a77eed9fc
kofi-teddy <agudogot@gmail.com>
2022-02-03 15:29:45 +0000

    added remove and iter methods

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -32,3 +32,30 @@ class Cart(object):
     def save(self):
         # mark the session as 'modified' to make sure it gets saved 
         self.session.modified = True
+    
+    def remove(self, product):
+        '''
+        Remove a product from the cart.
+        '''
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        '''
+        Iterate over the items in the cart and get the products 
+        products from the database.
+        '''
+        products_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=products_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
