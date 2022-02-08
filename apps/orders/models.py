9ab50915467794fd6dae810b402cabf6041d8b75
kofi-teddy <agudogot@gmail.com>
2022-02-08 00:08:24 +0000

    added order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from apps.shop.models import Product
+
+
+class Order(models.Model):
+    firt_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.item.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
